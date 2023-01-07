16942574395088432de3c7584efe1581d636a118
geoid40 <121327119+geoid40@users.noreply.github.com>
2023-01-07 10:05:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,52 +16,14 @@
         <div class="m">div02</div>
         <div class="y">div03</div>
     </div>
-   <!--  <div class="arabic"> اسلام</div> -->
-
-    <pre>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </pre>
+  <!--  php -->
+  <form action="" method="GET" name="">
+	<table>
+		<tr>
+			<td><input type="text" name="k" value="<?php echo isset($_GET['k']) ? $_GET['k'] : ''; ?>" placeholder="Enter your search keywords" /></td>
+			<td><input type="submit" name="" value="Search" /></td>
+		</tr>
+	</table>
+</form>
 </body>
 </html>
